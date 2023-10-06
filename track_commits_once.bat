55b0a91c0843c07ea4be7bfff39f75a6ba988ff2
dreamhigh9990 <dreamhigh9990@gmail.com>
2023-10-06 10:05:52 +0900

    update

--- track_commits_once.bat
+++ track_commits_once.bat
@@ -4,9 +4,9 @@ REM Set your Git username
 set YOUR_NAME=Your Name
 
 REM Set the output file
-set OUTPUT_FILE=commit_tracking.txt
+set OUTPUT_FILE=push_tracking_daily.txt
 
-REM Set the tracking start year
+REM Set the tracking start and end years
 set TRACKING_START_YEAR=2015
 set TRACKING_END_YEAR=2023
 
@@ -15,11 +15,19 @@ echo. > %OUTPUT_FILE%
 
 REM Loop through each year in the tracking period
 for /l %%Y in (%TRACKING_START_YEAR%,1,%TRACKING_END_YEAR%) do (
-    REM Count commits for the current year
-    for /f %%A in ('git log --since="%%Y-01-01" --until="%%Y-12-31" --author="%YOUR_NAME%" --oneline ^| find /c /v ""') do set COUNT=%%A
-    
-    REM Append the count to the output file
-    echo Contributions in year %%Y: !COUNT! >> %OUTPUT_FILE%
+    REM Loop through each day of the year
+    for /l %%D in (1,1,365) do (
+        setlocal enabledelayedexpansion
+        REM Calculate the date for the current day
+        set "DATE=%%Y-01-01 + %%D days"
+        endlocal
+        
+        REM Count pushes for the current day
+        for /f %%A in ('git rev-list --count --since="%DATE%" --until="%DATE% + 1 day" --author="%YOUR_NAME%" HEAD') do set COUNT=%%A
+        
+        REM Append the count to the output file
+        echo Pushes on %%DATE%: !COUNT! >> %OUTPUT_FILE%
+    )
 )
 
-echo Tracking completed for the specified period.
+echo Daily push tracking completed for the specified period.
